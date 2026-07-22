c6a81263d29cd0bd9b9c3393070c3e03154a25ac
Satyajit panda <141940014+satya-99928@users.noreply.github.com>
2026-07-22 15:20:38 +0530

    Time: 5 ms (36.53%), Space: 19.5 MB (9.53%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,17 +1,20 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        seen=set()
         n=len(board)
+        row=[set() for _ in range(n)]
+        col=[set() for _ in range(n)]
+        box=[set() for _ in range(n)]
         for r in range(n):
             for c in range(n):
                 val=board[r][c]
-                if val!=".":
-                    row=f"row {r} has {val}"
-                    col=f"col {c} has {val}"
-                    box=f"box {r//3}-{c//3} has {val}"
-                    if row in seen or col in seen or box in seen:
-                        return False
-                    seen.update([row,col,box])
+                if val==".":
+                    continue
+                box_idx=(r//3)*3+(c//3)
+                if val in row[r] or val in col[c] or val in box[box_idx]:
+                    return False
+                row[r].add(val)
+                col[c].add(val)
+                box[box_idx].add(val)
         return True
 
         

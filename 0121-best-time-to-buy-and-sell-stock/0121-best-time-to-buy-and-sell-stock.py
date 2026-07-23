416f30ac21fdd1f90bd704f90a5922648667764c
Satyajit panda <141940014+satya-99928@users.noreply.github.com>
2026-07-24 02:21:08 +0530

    Time: 19 ms (95.97%), Space: 28.8 MB (19.94%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -7,7 +7,7 @@ class Solution:
         for i in prices:
             if i<min_price:
                 min_price=i
-            elif i-min_price>max_profit:
+            if i-min_price>max_profit:
                 max_profit=i-min_price
         return max_profit
                 

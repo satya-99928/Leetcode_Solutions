f59f5a159d4225c91182470c9e62beeac7ca0fc3
Satyajit panda <141940014+satya-99928@users.noreply.github.com>
2026-08-05 20:03:45 +0530

    Time: 61 ms (73.35%), Space: 19.4 MB (16.54%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,9 +1,6 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        res=[0]*(2*n)
-        res[::2]=nums[:n]
-        res[1::2]=nums[n:]
-        return res
+        return [val for pairs in zip(nums[:n],nums[n:])for val in pairs]
 
         
 
